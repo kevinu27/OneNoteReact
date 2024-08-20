29fc3a60c88282fd988fd740947f5d58819b64c2
unknown <kevin.sm89@gmail.com>
2024-08-21 00:38:24 +0200

    select o button mejorado

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -5,7 +5,7 @@ import {CanvasContext} from '../context/canvas-context.jsx'
 
 const ButtonsContent = [
     {buttonId: "drawing", buttonType:'button'},
-    {buttonId: "stroke", buttonType:'select'}
+    {buttonId: "stroke", buttonType: 'select', options: ['Solid', 'Dashed', 'Dotted'] }
     
 ]
 
@@ -22,7 +22,7 @@ function onclickHandler(e, index, button){
     return(
         <>
             {ButtonsContent.map( (button, index) =>  button.buttonType == "button" ? <button onClick={(e)=> onclickHandler(e, index, button)} key={index}> {button.buttonId}</button> : null )}
-            {ButtonsContent.map( (button, index) =>  button.buttonType == "select" ? <select onClick={(e)=> onclickHandler(e, index, button)} key={index}> {button.buttonId}</select> : null )}
+            {ButtonsContent.map( (button, index) =>  button.buttonType == "select" ? <> <label>{button.buttonId}</label>  <select key={index} onChange={(e) => onclickHandler(e, index, button)}> {button.options.map((option, optionIndex) => ( <option key={optionIndex} value={option}> {option}</option>))}</select></> : null)}
         </>
     )
 }
